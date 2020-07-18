def6a61267f5d5f0d8bd6ac14e5ac064f54a95c3
pshoxxx <60489201+pshoxxx@users.noreply.github.com>
2020-07-18 14:30:48 -0700

    added in function and pseudocode for custom range port scan

--- portscanner.py
+++ portscanner.py
@@ -54,6 +54,55 @@ def common_scanner():
     # Printing the information to screen
     print('Scanning Completed in: ', total)
 
-common_scanner()
+# common_scanner()
+
+# Function to scan ranges of ports
+def custom_scanner():
+
+# Add two variables to specify ranges of ports to scan
 
 
+# Ask for input
+    remoteServer    = input("Enter a remote host to scan: ")
+    remoteServerIP  = socket.gethostbyname(remoteServer)
+
+    # Print a nice banner with information on which host we are about to scan
+    print("-" * 60)
+    print("Please wait, scanning remote host", remoteServerIP)
+    print("-" * 60)
+
+    # Check what time the scan started
+    t1 = datetime.now()
+
+    # Using the range function to specify ports (Use range of two variables)
+
+    # We also put in some error handling for catching errors
+
+    try:
+        for port in range():
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result == 0:
+                print("Port {}: 	 Open".format(port))
+            sock.close()
+
+    except KeyboardInterrupt:
+        print("You pressed Ctrl+C")
+        sys.exit()
+
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    except socket.error:
+        print("Couldn't connect to server")
+        sys.exit()
+
+    # Checking the time again
+    t2 = datetime.now()
+
+    # Calculates the difference of time, to see how long it took to run the script
+    total =  t2 - t1
+
+    # Printing the information to screen
+    print('Scanning Completed in: ', total)
