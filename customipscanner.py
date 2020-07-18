f91c2297f14c450dc4cc5fcd70b25baca90ad08a
pshoxxx <60489201+pshoxxx@users.noreply.github.com>
2020-07-18 14:54:10 -0700

    custom ip scanner file

---
+++ customipscanner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import ipaddress
+
+# Function to scan a range of ip addresses
+
+def custom_ip_scan():
+    # Ask for input of range of ips to scan in cidr notation
+
+    # Ask which ports will be scanned
+
+    # Redirect to appropriate functions
