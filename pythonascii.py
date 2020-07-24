01af8746dd4ee302d54bff4e79999c84213b6704
Alex Jensen <alexjensen@live.com>
2020-07-23 19:59:40 -0700

    Create pythonascii.py
    Random number gen w/ Ascii Art attached to each number

---
+++ pythonascii.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# seed the pseudorandom number generator
+from random import seed
+from random import randint
+# seed random number generator
+seed()
+# generate some random numbers
+randomnumber = randint(1,4)
+if randomnumber == 1:
+
+    print(r'''
+   __   _
+ _(  )_( )_
+(_   _    _)
+  (_) (__)
+
+            ''')
+
+if randomnumber == 2:
+    print(r'''
+                                                                                         
+FFFFFFFFFFFFFFFFFFFFFF      AAA               RRRRRRRRRRRRRRRRR   TTTTTTTTTTTTTTTTTTTTTTT
+F::::::::::::::::::::F     A:::A              R::::::::::::::::R  T:::::::::::::::::::::T
+F::::::::::::::::::::F    A:::::A             R::::::RRRRRR:::::R T:::::::::::::::::::::T
+FF::::::FFFFFFFFF::::F   A:::::::A            RR:::::R     R:::::RT:::::TT:::::::TT:::::T
+  F:::::F       FFFFFF  A:::::::::A             R::::R     R:::::RTTTTTT  T:::::T  TTTTTT
+  F:::::F              A:::::A:::::A            R::::R     R:::::R        T:::::T        
+  F::::::FFFFFFFFFF   A:::::A A:::::A           R::::RRRRRR:::::R         T:::::T        
+  F:::::::::::::::F  A:::::A   A:::::A          R:::::::::::::RR          T:::::T        
+  F:::::::::::::::F A:::::A     A:::::A         R::::RRRRRR:::::R         T:::::T        
+  F::::::FFFFFFFFFFA:::::AAAAAAAAA:::::A        R::::R     R:::::R        T:::::T        
+  F:::::F         A:::::::::::::::::::::A       R::::R     R:::::R        T:::::T        
+  F:::::F        A:::::AAAAAAAAAAAAA:::::A      R::::R     R:::::R        T:::::T        
+FF:::::::FF     A:::::A             A:::::A   RR:::::R     R:::::R      TT:::::::TT      
+F::::::::FF    A:::::A               A:::::A  R::::::R     R:::::R      T:::::::::T      
+F::::::::FF   A:::::A                 A:::::A R::::::R     R:::::R      T:::::::::T      
+FFFFFFFFFFF  AAAAAAA                   AAAAAAARRRRRRRR     RRRRRRR      TTTTTTTTTTT      
+                                                  
+    ''')
+
+if randomnumber == 3:
+        print(r'''
+                        
+______                        ___      _                               _  ______                       _____           _ 
+|  ___|                      / _ \    | |                             | | | ___ \                     |_   _|         | |
+| |_ __ _ _ __   ___ _   _  / /_\ \ __| |_   ____ _ _ __   ___ ___  __| | | |_/ /___  ___ ___  _ __     | | ___   ___ | |
+|  _/ _` | '_ \ / __| | | | |  _  |/ _` \ \ / / _` | '_ \ / __/ _ \/ _` | |    // _ \/ __/ _ \| '_ \    | |/ _ \ / _ \| |
+| || (_| | | | | (__| |_| | | | | | (_| |\ V / (_| | | | | (_|  __/ (_| | | |\ \  __/ (_| (_) | | | |   | | (_) | (_) | |
+\_| \__,_|_| |_|\___|\__, | \_| |_/\__,_| \_/ \__,_|_| |_|\___\___|\__,_| \_| \_\___|\___\___/|_| |_|   \_/\___/ \___/|_|
+                      __/ |                                                                                              
+                     |___/                                                                                               
+        
+        ''')
+
+if randomnumber == 4:
+    print(r'''
+ _________________
+|# :           : #|
+|  :           :  |
+|  :           :  |
+|  :           :  |
+|  :___________:  |
+|     _________   |
+|    | __      |  |
+|    ||  |     |  |
+\____||__|_____|__|
+    
+    
+    ''')
